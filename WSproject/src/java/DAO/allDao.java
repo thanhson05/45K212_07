22dcbeb88a6ef9baa8e115dcd7e5ce94abdf0179
Nguyen Van Tu <85673836+tusept@users.noreply.github.com>
2022-04-20 08:59:36 +0700

    Update allDao.java

--- WSproject/src/java/DAO/allDao.java
+++ WSproject/src/java/DAO/allDao.java
@@ -35,6 +35,23 @@ public class allDao extends DBContext {
         return list;
     }
 
+    public User getUsersById(int id) {
+        try {
+            String sql = "select*from Users where id=?";
+            stm = connection.prepareCall(sql);
+            stm.setInt(1, id);
+            rs = stm.executeQuery();
+            while (rs.next()) {
+                User u = new User(rs.getInt("id"), rs.getString("username"), rs.getString("pass"),
+                        rs.getString("name"), rs.getString("phone"), rs.getString("address"), rs.getString("isAmin"));
+                return u;
+
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
     public ArrayList<Staff> getAllStaff() {
         ArrayList<Staff> list = new ArrayList<>();
         try {
@@ -67,6 +84,22 @@ public class allDao extends DBContext {
         return null;
     }
 
+    public ArrayList<Staff> searchByName(String name) {
+        ArrayList<Staff> list = new ArrayList<>();
+        try {
+            String sql = "  select * from Staff where Name like ?";
+            stm = connection.prepareCall(sql);
+            stm.setString(1, "%" + name + "%");
+            rs = stm.executeQuery();
+            while (rs.next()) {
+                list.add(new Staff(rs.getInt("id"), rs.getString("Name"), rs.getString("phone"),
+                        rs.getString("addresss"), rs.getString("imglink")));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
     public ArrayList<Staff> get3Staff() {
         ArrayList<Staff> list = new ArrayList<>();
         try {
@@ -85,12 +118,13 @@ public class allDao extends DBContext {
     public ArrayList<Transaction> getAllTransactions() {
         ArrayList<Transaction> list = new ArrayList<>();
         try {
-            String sql = "select*from Transactions ";
+            String sql = "select t.*, u.name as username, s.Name as sname from Transactions t, Users u, Staff s \n"
+                    + "where t.Staffid = s.id and t.Userid = u.id";
             stm = connection.prepareCall(sql);
             rs = stm.executeQuery();
             while (rs.next()) {
-//                list.add(new Transaction(rs.getInt("id"), rs.getString("username"), rs.getString("pass"),
-//                        rs.getString("name"), rs.getString("phone"), rs.getString("address"), rs.getString("isAmin")));
+                list.add(new Transaction(rs.getInt("id"), new User(rs.getString("username")), new Staff(rs.getString("sname")),
+                        rs.getInt("totaltime"), rs.getString("total"), rs.getString("status")));
             }
         } catch (Exception e) {
         }
@@ -133,6 +167,40 @@ public class allDao extends DBContext {
         }
     }
 
+    public void deleteTrans(int id) {
+        try {
+            String sql = "DELETE FROM Transactions WHERE Id = ?";
+            stm = connection.prepareCall(sql);
+            stm.setInt(1, id);
+            stm.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void deleteUser(int id) {
+        try {
+            String sql = "delete from Transactions where Userid = ?  DELETE FROM Users WHERE id = ?";
+            stm = connection.prepareCall(sql);
+            stm.setInt(1, id);
+            stm.setInt(2, id);
+
+            stm.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void deleteStaff(int id) {
+        try {
+            String sql = "delete from Transactions where Staffid = ?  DELETE FROM Staff WHERE id = ?";
+            stm = connection.prepareCall(sql);
+            stm.setInt(1, id);
+            stm.setInt(2, id);
+
+            stm.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
     public void insertTrans(int sid, int uid, int totaltime, String status) {
         try {
             String sql = "insert into Transactions(Userid,Staffid,totaltime,total,status) values(?,?,?,?,?);";
@@ -147,7 +215,7 @@ public class allDao extends DBContext {
         }
     }
 
-    public void insertUser( String user,String pass,String name,String phone,String address) {
+    public void insertUser(String user, String pass, String name, String phone, String address) {
         try {
             String sql = "insert into Users(username,pass, name, phone,address,isAmin) values(?,?,?,?,?,'false');";
             stm = connection.prepareCall(sql);
@@ -156,6 +224,47 @@ public class allDao extends DBContext {
             stm.setString(3, name);
             stm.setString(4, phone);
             stm.setString(5, address);
+            stm.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void insertStaff(String name, String phone, String address, String img) {
+        try {
+            String sql = "insert into Staff(Name, phone,addresss,imglink) values(?,?,?,?);";
+            stm = connection.prepareCall(sql);
+            stm.setString(1, name);
+            stm.setString(2, phone);
+            stm.setString(3, address);
+            stm.setString(4, img);
+            stm.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void updateStaff(String name, String phone, String address, String img, int id) {
+        try {
+            String sql = "update Staff Set Name=?, phone=?,addresss=?,imglink=? where id=?";
+            stm = connection.prepareCall(sql);
+            stm.setString(1, name);
+            stm.setString(2, phone);
+            stm.setString(3, address);
+            stm.setString(4, img);
+            stm.setInt(5, id);
+            stm.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void updateUser(String username, String name, String phone, String address, int id) {
+        try {
+            String sql = "  update Users Set username=?, name=?,address=?,phone=? where id=? ";
+            stm = connection.prepareCall(sql);
+            stm.setString(1, username);
+            stm.setString(2, name);
+            stm.setString(3, address);
+            stm.setString(4, phone);
+            stm.setInt(5, id);
             stm.executeUpdate();
         } catch (Exception e) {
         }
