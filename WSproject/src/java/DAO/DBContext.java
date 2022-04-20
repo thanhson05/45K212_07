135bd33577e20a6d055e2769b42bc3c6544308ae
Nguyen Van Tu <85673836+tusept@users.noreply.github.com>
2022-04-20 08:59:40 +0700

    Update DBContext.java

--- WSproject/src/java/DAO/DBContext.java
+++ WSproject/src/java/DAO/DBContext.java
@@ -18,7 +18,7 @@ public class DBContext {
         try {
             String user = "sa";
             String pass = "123456";
-            String url = "jdbc:sqlserver://DESKTOP-VEQ8329:1433;databaseName=DL";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=DL";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (Exception ex) {
